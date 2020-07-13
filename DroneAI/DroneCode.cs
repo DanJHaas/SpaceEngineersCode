876efb9ba417c5bae0d38979b34e976c7913f55e
HattyH124 <blueeyedan07@gmail.com>
2020-07-13 06:47:50 -0400

    Commit 1

--- DroneAI/DroneCode.cs
+++ DroneAI/DroneCode.cs
@@ -38,7 +38,7 @@ namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor
 
         public Program()
         {
-            //testing git
+            //testing git again
             Runtime.UpdateFrequency = UpdateFrequency.Update10;
             info = GridTerminalSystem.GetBlockWithName("Lcd") as IMyTextPanel;
             brain = GridTerminalSystem.GetBlockWithName("controller") as IMyRemoteControl;
